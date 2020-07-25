0c310f62405c976ef8aa4061720667e59412d3e2
clstanton <cstanton77@gmail.com>
2020-07-24 19:30:44 -0500

    connect front to back

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// set up Mongoose to connect when we start the app
+const mongoose = require('mongoose');
+
 const express = require('express');
 
 const app = express();
@@ -8,5 +11,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
+
+// mongoose.connect() tells Mongoose which database we want to connect to. If the environment variable MONGODB_URI exists, like on Heroku when we deploy later, it will use that. Otherwise, it will short-circuit to the local MongoDB server's database at mongodb://localhost/pizza-hunt. The second argument in the example is a set of configuration options Mongoose asks for more information about.
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
